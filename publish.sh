620fe3727d8aec9cde197ee756724559147f035c
Justin Ellis <justin.ellis18@gmail.com>
2017-11-18 14:10:15 -0500

    Publishing to master(publish.sh)

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+
+echo "Generating site"
+hugo
+
+echo "Updating master branch"
+cd public && git add --all
+cd ../
+git mv public/* .
+git rm public
+git commit -m "Publishing to master(publish.sh)"
